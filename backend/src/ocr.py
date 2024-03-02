bac6f98495289c8cbedbf17b410c2dc5257ff283
gracexkan <80564637+gracexkan@users.noreply.github.com>
2024-03-02 19:21:42 +1100

    feat(backend): tidied comments

--- backend/src/ocr.py
+++ backend/src/ocr.py
@@ -4,12 +4,8 @@ from google.api_core.client_options import ClientOptions
 from google.cloud import documentai  # type: ignore
 
 project_id = "wit-ocr"
-location = "us" # Format is "us" or "eu"
-processor_id = "f943e0aab3ed0a34" # Create processor before running sample
-# file_path = "/path/to/local/pdf"
-# mime_type = "application/pdf" # Refer to https://cloud.google.com/document-ai/docs/file-types for supported file types
-# field_mask = "text,entities,pages.pageNumber"  # Optional. The fields to return in the Document object.
-# processor_version_id = "YOUR_PROCESSOR_VERSION_ID" # Optional. Processor version to use
+location = "us"
+processor_id = "f943e0aab3ed0a34"
 
 def process_document_sample(
     file_path: str,
@@ -17,39 +13,28 @@ def process_document_sample(
     field_mask: Optional[str] = None,
     processor_version_id: Optional[str] = None,
 ) -> None:
-    # You must set the `api_endpoint` if you use a location other than "us".
     opts = ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
 
     client = documentai.DocumentProcessorServiceClient(client_options=opts)
 
     if processor_version_id:
-        # The full resource name of the processor version, e.g.:
-        # `projects/{project_id}/locations/{location}/processors/{processor_id}/processorVersions/{processor_version_id}`
         name = client.processor_version_path(
             project_id, location, processor_id, processor_version_id
         )
     else:
-        # The full resource name of the processor, e.g.:
-        # `projects/{project_id}/locations/{location}/processors/{processor_id}`
         name = client.processor_path(project_id, location, processor_id)
 
-    # Read the file into memory
     with open(file_path, "rb") as image:
         image_content = image.read()
 
-    # Load binary data
     raw_document = documentai.RawDocument(content=image_content, mime_type=mime_type)
 
-    # For more information: https://cloud.google.com/document-ai/docs/reference/rest/v1/ProcessOptions
-    # Optional: Additional configurations for processing.
     process_options = documentai.ProcessOptions(
-        # Process only specific pages
         individual_page_selector=documentai.ProcessOptions.IndividualPageSelector(
             pages=[1]
         )
     )
 
-    # Configure the process request
     request = documentai.ProcessRequest(
         name=name,
         raw_document=raw_document,
@@ -59,10 +44,6 @@ def process_document_sample(
 
     result = client.process_document(request=request)
 
-    # For a full list of `Document` object attributes, reference this page:
-    # https://cloud.google.com/document-ai/docs/reference/rest/v1/Document
     document = result.document
 
-    # Read the text recognition output from the processor
-    print("The document contains the following text:")
     return document.text
