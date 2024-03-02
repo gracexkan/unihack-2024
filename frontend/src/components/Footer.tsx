437eb6615adc45f676e7c56657a1456a54465aba
gracexkan <80564637+gracexkan@users.noreply.github.com>
2024-03-03 01:45:27 +1100

    feat(frontend): removed an icon on the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -10,22 +10,19 @@ const Footer = () => {
   const navigate = useNavigate();
   return (
     <div className="fixed w-screen bottom-0 left-0 right-0 z-40 bg-white backdrop-blur py-3 flex justify-between items-center border-t border-slate-900/1">
-      <div className="pl-10">
-        <IconButton title="Scan barcode" onClick={() => navigate("/barcode")}>
+      <div className="pr-10">
+        <IconButton title="Add prescription" onClick={() => navigate("/prescription")}>
           <CameraOutlined />
         </IconButton>
       </div>
-      <div>
-        <IconButton title="Add prescription" onClick={() => navigate("/prescription")}>
-          <MedicineBoxOutlined />
+      <div className="pr-10">
+        <IconButton title="Access profile" onClick={() => navigate("/profiles")}>
+          <TeamOutlined />
         </IconButton>
       </div>
-      <IconButton title="Access profile" onClick={() => navigate("/profiles")}>
-        <TeamOutlined />
-      </IconButton>
       <div className="pr-10">
         <IconButton title="Edit preferences" onClick={() => navigate("/preferences")}>
-          <TeamOutlined />
+        <MedicineBoxOutlined />
         </IconButton>
       </div>
     </div>
