88882844bb18b0ef7b98e1313bc0bae2c6d0b16c
gracexkan <80564637+gracexkan@users.noreply.github.com>
2024-03-03 04:53:53 +1100

    fix(frontend): made top padding larger

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import logo from '../assets/logo.png';
 const Navbar = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex justify-between items-center font-sans top-0 z-40 w-full backdrop-blur bg-white lg:z-50 py-2">
+    <div className="flex justify-between items-center font-sans top-0 z-40 w-full backdrop-blur bg-white lg:z-50 py-2 mb-5">
       <div className="px-8 font-semibold mt-2">
         <Link to="/"><img width={50} src={logo} alt="Description" /></Link>
       </div>
